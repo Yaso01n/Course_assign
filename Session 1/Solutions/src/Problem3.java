944671136fbab791d769ecb592c5954d77a5377a
Yaso01n <87495750+Yaso01n@users.noreply.github.com>
2021-07-23 17:15:24 +0200

    Update Problem3.java

--- Session 1/Solutions/src/Problem3.java
+++ Session 1/Solutions/src/Problem3.java
@@ -1,5 +1,23 @@
 public class Problem3 {
     public static void main(String [] args){
         // Write your code here
-    }
+        Scanner sc =new Scanner(System.in);
+		int q= sc.nextInt();
+		for(int i=0;i<q;i++) {
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		int n= sc.nextInt();
+	        int sum = a;
+			for(int k=0;k<=n-1;k++) {
+				sum = sum +(pow(2,k)*b);
+				System.out.print(sum+" ");
+			}	
+			System.out.printf("%n"); 
+		}
+		}
+
+	private static int pow(int i, int k) {
+
+		return (int) Math.pow(i, k);
+	}
 }
