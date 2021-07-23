5c556063b9284efe89878d66ed898d9357d8e1e6
Yaso01n <87495750+Yaso01n@users.noreply.github.com>
2021-07-23 17:12:49 +0200

    Update Problem2.java

--- Session 1/Solutions/src/Problem2.java
+++ Session 1/Solutions/src/Problem2.java
@@ -1,5 +1,12 @@
 public class Problem2 {
     public static void main(String [] args){
         // Write your code here
+        Scanner sc =new Scanner(System.in);
+       System.out.println("================================");
+            for(int i=0;i<3;i++){
+		System.out.printf("%-15s%03d%n",sc.next(),sc.nextInt());
+	}
+            System.out.println("================================");
+
     }
 }
