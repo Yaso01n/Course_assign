ee8228c419bd716b4b9c492844ae625386a002eb
Yaso01n <87495750+Yaso01n@users.noreply.github.com>
2021-07-23 20:07:54 +0200

    Update Bonus Problem.java

--- Session 1/Solutions/src/Bonus Problem.java
+++ Session 1/Solutions/src/Bonus Problem.java
@@ -1,5 +1,18 @@
 public class Problem4 {
     public static void main(String [] args){
         // Write your code here
+        Scanner sc =new Scanner(System.in);
+	 int a= sc.nextInt();
+	 String s = sc.next();
+	 int i=a;
+		 for(int k=0;k<a;k++) { 
+			 if (i%2==0) {
+				 System.out.print(s.charAt(i/2-1));	
+	} 
+	 else {
+		 System.out.print(s.charAt(i/2));
+		}
+			 i--;
+	 }
     }
 }
