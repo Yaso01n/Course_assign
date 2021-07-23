34841c734bc43736c4fe00f2d58dde9b4cd9335a
Yaso01n <87495750+Yaso01n@users.noreply.github.com>
2021-07-23 17:11:18 +0200

    Update Problem1.java

--- Session 1/Solutions/src/Problem1.java
+++ Session 1/Solutions/src/Problem1.java
@@ -1,5 +1,15 @@
 public class Problem1 {
     public static void main(String [] args){
         // Write your code here
+        Scanner input =new Scanner(System.in);
+		int a= input.nextInt();
+		int b= input.nextInt();
+		int year=0;
+		while(a<=b) {
+	     a = a*3;
+	     b = b*2;
+	    year++;		 
+		}
+		System.out.print(year);
     }
 }
